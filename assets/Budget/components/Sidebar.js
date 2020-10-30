51e6eb5755f838b652538a20dfe98464e6e1a38d
Mohamed Millogo <mohamedmillogo1@gmail.com>
2020-10-30 19:32:27 +0000

    version00

--- assets/Budget/components/Sidebar.js
+++ assets/Budget/components/Sidebar.js
@@ -3,29 +3,76 @@ import React, { Component } from 'react';
 class Sidebar extends Component {
     render() {
         return (
-           <div className="bg-light border-right" id="sidebar-wrapper">
-  <div className="sidebar-heading">GAMP </div>
-  <div className="dropdown-divider" />
-  <div className="list-group list-group-flush">
-    <a href="#" className="list-group-item list-group-item-action bg-light"><h3>DAFB</h3></a>
-    <a href="#" className="dropdown-toggle list-group-item list-group-item-action bg-light" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Nomenclature</a>
-        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">Créer</a>
-            <a className="dropdown-item" href="#">Consulter</a>
-    </div>
-    <a href="#" className="dropdown-toggle list-group-item list-group-item-action bg-light" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catégorie</a>
-        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">Créer</a>
-            <a className="dropdown-item" href="#">Consulter</a>
-    </div>
-    <a href="#" className="dropdown-toggle list-group-item list-group-item-action bg-light" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ligne</a>
-        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">Créer</a>
-            <a className="dropdown-item" href="#">Consulter</a>
-    </div>
-    <div className="dropdown-divider" />
-  </div>
-</div>
+            <div className="pcoded-wrapper">
+            <nav className="pcoded-navbar">
+              <div className="pcoded-inner-navbar main-menu">
+                <div className="pcoded-navigatio-lavel"><h2></h2></div>
+                <div className="pcoded-navigatio-lavel"><h2>DA<span>FB</span></h2></div>
+                <ul className="pcoded-item pcoded-left-item">
+                  <li className="pcoded-hasmenu">
+                    <a href="javascript:void(0)">
+                      <span className="pcoded-micon"><i className="feather icon-book"></i></span>
+                      <span className="pcoded-mtext">Nomenclature</span>
+                    </a>
+                    <ul className="pcoded-submenu">
+                      <li className>
+                        <a href="/livre">
+                        <i className="feather icon-edit"></i>
+                          <span className="pcoded-mtext"> Créer</span>
+                        </a>
+                      </li>
+                      <li className>
+                        <a href="/livre/liste">
+                        <i className="feather icon-list"></i>
+                          <span className="pcoded-mtext">Consulter</span>
+                        </a>
+                      </li>
+                </ul>
+                </li>
+                <li className="pcoded-hasmenu">
+                    <a href="javascript:void(0)">
+                      <span className="pcoded-micon"><i className="feather icon-plus-square" /></span>
+                      <span className="pcoded-mtext">Catégorie</span>
+                    </a>
+                    <ul className="pcoded-submenu">
+                    <li className>
+                      <a href="/categorie">
+                      <i className="feather icon-edit"></i>
+                        <span className="pcoded-mtext"> Créer</span>
+                      </a>
+                    </li>
+                    <li className>
+                      <a href="/categories/liste">
+                      <i className="feather icon-list"></i>
+                        <span className="pcoded-mtext">Consulter</span>
+                      </a>
+                    </li>
+              </ul>
+              </li>
+              <li className="pcoded-hasmenu">
+              <a href="javascript:void(0)">
+                <span className="pcoded-micon"><i className="feather icon-list" /></span>
+                <span className="pcoded-mtext">Ligne</span>
+              </a>
+              <ul className="pcoded-submenu">
+              <li className>
+                <a href="/compte">
+                <i className="feather icon-edit"></i>
+                  <span className="pcoded-mtext"> Créer</span>
+                </a>
+              </li>
+              <li className>
+                <a href="/comptes/liste">
+                <i className="feather icon-list"></i>
+                  <span className="pcoded-mtext">Consulter</span>
+                </a>
+              </li>
+        </ul>
+        </li>
+              </ul>
+              </div>
+            </nav>
+          </div>
 
     );
     }
