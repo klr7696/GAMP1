982691f48d39c5dd5397f7db24f7d35c4eb0063d
Mohamed Millogo <mohamedmillogo1@gmail.com>
2020-11-01 12:32:01 +0000

    v2

--- assets/app.js
+++ assets/app.js
@@ -4,13 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import Sidebar from "./Budget/components/Sidebar";
 import Header from "./Budget/components/Header";
 import Content from './Budget/components/Content';
+import Load from "./Budget/components/Load";
 
 
 class App extends Component {
     render() {
         return (
         <BrowserRouter>
-           
+           <Load/>
             <div id="pcoded" className="pcoded">
                 <div className="pcoded-overlay-box"></div>
                     <div className="pcoded-container navbar-wrapper">
